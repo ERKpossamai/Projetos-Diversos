c1469989bccb1373996d8653695ce6dd1939b838
Erick Possamai <erick.lemos@escola.pr.gov.br>
2025-06-02 11:39:46 -0300

    sla

---
+++ Projetos diversos/C/mostrar_numero_int_na_tela.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Digite um número inteiro: ");
+    scanf("%d", &numero);
+
+    printf("O número digitado foi: %d\n", numero);
+
+    return 0;
+
+}
